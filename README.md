3a9d4fb625fddfa94fb4bc59691de00ab28c9647
Du Ang <du2ang233@gmail.com>
2019-05-14 13:43:36 +0800

    Add a test script to test the pytorch model and some test results

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 A PyTorch reimplementation for paper Generative Image Inpainting with Contextual Attention (https://arxiv.org/abs/1801.07892)
 
 
+## Train the model
+```bash
+python train.py --config configs/config.yaml
+```
+
+The checkpoints and logs will be saved to `checkpoints`。
+
+## Test with the trained model
+By default, it will load the latest saved model in the checkpoints. You can also use `--iter` to choose the saved models by iteration.
+
+```bash
+python test_single.py \
+	--image examples/imagenet/imagenet_patches_ILSVRC2012_val_00008210_input.png \
+	--mask examples/center_mask_256.png \
+	--output examples/output.png \
+```
+
+## Test with the converted TF model:
 Converted TF model: [[Link](https://drive.google.com/file/d/1vz2Qp12_iwOiuvLWspLHrC1UIuhSLojx/view?usp=sharing)]
-
-
-## Inpainting Example:
 
 ```bash
 python test_tf_model.py \
@@ -14,3 +29,15 @@ python test_tf_model.py \
 	--output examples/output.png \
 	--model-path torch_model.p
 ```
+
+## Test results on ImageNet validation set patches
+
+With PyTorch, the model was trained on ImageNet for 50k iterations to converge (about 120h). Here are some test results on the patches from ImageNet validation set.
+
+| Input | Inpainted |
+|:---:|:---:|
+| [![val_00000827_input](examples/imagenet/imagenet_patches_ILSVRC2012_val_00000827_input.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00000827_input.png)  | [![val_00000827_output](examples/imagenet/imagenet_patches_ILSVRC2012_val_00000827_output.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00000827_output.png) |
+| [![val_00008210_input](examples/imagenet/imagenet_patches_ILSVRC2012_val_00008210_input.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00008210_input.png)  | [![val_00008210_output](examples/imagenet/imagenet_patches_ILSVRC2012_val_00008210_output.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00008210_output.png) |
+| [![val_00022355_input](examples/imagenet/imagenet_patches_ILSVRC2012_val_00022355_input.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00022355_input.png)  | [![val_00022355_output](examples/imagenet/imagenet_patches_ILSVRC2012_val_00022355_output.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00022355_output.png) |
+| [![val_00025892_input](examples/imagenet/imagenet_patches_ILSVRC2012_val_00025892_input.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00025892_input.png)  | [![val_00025892_output](examples/imagenet/imagenet_patches_ILSVRC2012_val_00025892_output.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00025892_output.png) |
+| [![val_00045643_input](examples/imagenet/imagenet_patches_ILSVRC2012_val_00045643_input.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00045643_input.png)  | [![val_00045643_output](examples/imagenet/imagenet_patches_ILSVRC2012_val_00045643_output.png)](examples/imagenet/imagenet_patches_ILSVRC2012_val_00045643_output.png) |
---
+++ test_single.py
@@ -0,0 +1,122 @@
+import os
+import random
+from argparse import ArgumentParser
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+from model.networks import Generator
+from utils.tools import get_config, random_bbox, mask_image, is_image_file, default_loader, normalize, get_model_list
+
+
+parser = ArgumentParser()
+parser.add_argument('--config', type=str, default='configs/config.yaml',
+                    help="training configuration")
+parser.add_argument('--seed', type=int, help='manual seed')
+parser.add_argument('--image', type=str)
+parser.add_argument('--mask', type=str, default='')
+parser.add_argument('--output', type=str, default='output.png')
+parser.add_argument('--flow', type=str, default='')
+parser.add_argument('--gpu_ids', type=str, default='2')
+parser.add_argument('--checkpoint_path', type=str, default='')
+parser.add_argument('--iter', type=int, default=0)
+
+def main():
+    args = parser.parse_args()
+    config = get_config(args.config)
+
+    # CUDA configuration
+    cuda = config['cuda']
+    device_ids = [int(i.strip()) for i in args.gpu_ids.split(',')]
+    if cuda:
+        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(i) for i in device_ids)
+        device_ids = list(range(len(device_ids)))
+        config['gpu_ids'] = device_ids
+        cudnn.benchmark = True
+
+    print("Arguments: {}".format(args))
+
+    # Set random seed
+    if args.seed is None:
+        args.seed = random.randint(1, 10000)
+    print("Random seed: {}".format(args.seed))
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if cuda:
+        torch.cuda.manual_seed_all(args.seed)
+
+    print("Configuration: {}".format(config))
+
+    try:  # for unexpected error logging
+        with torch.no_grad():   # enter no grad context
+            if is_image_file(args.image):
+                if args.mask and is_image_file(args.mask):
+                    # Test a single masked image with a given mask
+                    x = default_loader(args.image)
+                    mask = default_loader(args.mask)
+                    x = transforms.Resize(config['image_shape'][:-1])(x)
+                    x = transforms.CenterCrop(config['image_shape'][:-1])(x)
+                    mask = transforms.Resize(config['image_shape'][:-1])(mask)
+                    mask = transforms.CenterCrop(config['image_shape'][:-1])(mask)
+                    x = transforms.ToTensor()(x)
+                    mask = transforms.ToTensor()(mask)[0].unsqueeze(dim=0)
+                    x = normalize(x)
+                    x = x * (1. - mask)
+                    x = x.unsqueeze(dim=0)
+                    mask = mask.unsqueeze(dim=0)
+                elif args.mask:
+                    raise TypeError("{} is not an image file.".format(args.mask))
+                else:
+                    # Test a single ground-truth image with a random mask
+                    ground_truth = default_loader(args.image)
+                    ground_truth = transforms.Resize(config['image_shape'][:-1])(ground_truth)
+                    ground_truth = transforms.CenterCrop(config['image_shape'][:-1])(ground_truth)
+                    ground_truth = transforms.ToTensor()(ground_truth)
+                    ground_truth = normalize(ground_truth)
+                    ground_truth = ground_truth.unsqueeze(dim=0)
+                    bboxes = random_bbox(config, batch_size=ground_truth.size(0))
+                    x, mask = mask_image(ground_truth, bboxes, config)
+
+                # Set checkpoint path
+                if not args.checkpoint_path:
+                    checkpoint_path = os.path.join('checkpoints',
+                                                   config['dataset_name'],
+                                                   config['mask_type'] + '_' + config['expname'])
+                else:
+                    checkpoint_path = args.checkpoint_path
+
+                # Define the trainer
+                netG = Generator(config['netG'], cuda, device_ids)
+                # Resume weight
+                last_model_name = get_model_list(checkpoint_path, "gen", iteration=args.iter)
+                netG.load_state_dict(torch.load(last_model_name))
+                model_iteration = int(last_model_name[-11:-3])
+                print("Resume from {} at iteration {}".format(checkpoint_path, model_iteration))
+
+                if cuda:
+                    netG = nn.parallel.DataParallel(netG, device_ids=device_ids)
+                    x = x.cuda()
+                    mask = mask.cuda()
+
+                # Inference
+                x1, x2, offset_flow = netG(x, mask)
+                inpainted_result = x2 * mask + x * (1. - mask)
+
+                vutils.save_image(inpainted_result, args.output, padding=0, normalize=True)
+                print("Saved the inpainted result to {}".format(args.output))
+                if args.flow:
+                    vutils.save_image(offset_flow, args.flow, padding=0, normalize=True)
+                    print("Saved offset flow to {}".format(args.flow))
+            else:
+                raise TypeError("{} is not an image file.".format)
+        # exit no grad context
+    except Exception as e:  # for unexpected error logging
+        print("Error: {}".format(e))
+        raise e
+
+
+if __name__ == '__main__':
+    main()
